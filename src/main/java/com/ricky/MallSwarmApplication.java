2ce5b18573f6e6322462f5d8762517e55170b65b
richchen <809155736@qq.com>
2020-05-08 20:50:55 +0800

    init project

---
+++ src/main/java/com/ricky/MallSwarmApplication.java
@@ -0,0 +1,13 @@
+package com.ricky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallSwarmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallSwarmApplication.class, args);
+    }
+
+}
